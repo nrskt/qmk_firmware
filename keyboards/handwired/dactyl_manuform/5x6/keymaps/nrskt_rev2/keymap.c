60846a728ae9f91285f8a95983671a61493e5194
nrskt <norisuke_takafuji@caddi.jp>
2021-08-09 03:57:51 +0900

    Update

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/nrskt_rev2/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/nrskt_rev2/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,         KC_5,                  KC_6,    KC_7,    KC_8,    KC_9,        KC_0,    KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,         KC_T,                  KC_Y,    KC_U,    KC_I,    KC_O,        KC_P,    KC_MINS, 
         KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,         KC_G,                  KC_H,    KC_J,    KC_K,    KC_L,        KC_SCLN, KC_QUOT, 
-        RESET,   KC_Z,    KC_X,    KC_C,    KC_V,         KC_B,                  KC_N,    KC_M,    KC_COMM, KC_DOT,      KC_SLSH, KC_BSLS, 
+        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,         KC_B,                  KC_N,    KC_M,    KC_COMM, KC_DOT,      KC_SLSH, KC_BSLS, 
         _______, _______, KC_LBRC, KC_GRV,  _______,      _______,               _______, _______, _______, _______,     _______, KC_SPC,
         _______, _______, TAP_SPC, CTL_SFT, TD(CT_A_F10), RSFT_T(KC_BSPC),       XM_SFT,  LY_12,   LY_23,   TD(CT_S_F7), KC_ENT,  _______),
 
